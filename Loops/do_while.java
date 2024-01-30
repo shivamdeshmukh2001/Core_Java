f32b111238ad9a98147d1b7fdedc57c25f28e3bb
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-01-30 14:21:39 +0530

    Do while loop

---
+++ Loops/do_while.java
@@ -0,0 +1,12 @@
+public class do_while {
+    public static void main(String[] args) {
+        //Print 1 to 10 numbers
+        int i =1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+    }
+}
+
+// do while loop execute at least once as condition true or false
