fc36657500433422443deba376cf167980f42317
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-01-30 14:13:20 +0530

    while loop

---
+++ Loops/while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String [] args){
+        //print 1 to 100
+        int i = 1;
+        while (i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
