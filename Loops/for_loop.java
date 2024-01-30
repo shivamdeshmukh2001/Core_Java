4f11104208b6512744182570153376d023a4e0a0
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-01-30 14:06:20 +0530

    Java For loop

--- Loops/for_loop.java
+++ Loops/for_loop.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class for_loop{
     public static void main(String [] args){
-        //Print 1 to 
+        //Print table of 2
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Table of ");
+        int n = sc.nextInt();
+        for (int i=1;i<=10;i++){
+            
+            System.out.println(n*i);
+        } 
     }
 }
