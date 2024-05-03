1de5dca5ae860d1573b7dd01542ed4bcbcaef6d2
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-05-03 13:29:16 +0530

    Create nineteenTable.java

---
+++ Loops/for loop/nineteenTable.java
@@ -0,0 +1,11 @@
+public class nineteenTable {
+    public static void main(String[] args) {
+        int n = 19;
+        int t=0;
+
+        for(int i=1;i<11;i++){
+            t=n*i;
+            System.out.println(n + " * " + i+ "  =  " + t);
+        }
+    }
+}
