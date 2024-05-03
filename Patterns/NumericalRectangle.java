3b1ed10501c09c93008c664004d2b8ae062ccb46
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-05-04 02:01:53 +0530

    pattern

---
+++ Patterns/NumericalRectangle.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class NumericalRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no :");
+        int r = sc.nextInt();
+
+
+        for(int i= 1; i <= r;i++){
+            for (int j = i; j <= r; j++) {
+                System.out.print(j);
+                
+            }
+
+            for(int k = 1; k <= i-1; k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
