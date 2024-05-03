2058026ac556683ea1a2f99e5b784736b1d1bb84
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-05-03 13:50:17 +0530

    loop

--- Loops/while loop/while_loop.java
+++ Loops/while loop/while_loop.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class while_loop {
     public static void main(String [] args){
-        //print 1 to 100
+        //print 1 to n
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number :");
+        int n = sc.nextInt();
         int i = 1;
-        while (i <= 100){
-            System.out.println(i);
+        while (i <= n){
+            System.out.println(i ); 
             i++;
         }
+    sc.close();
     }
+
 }
