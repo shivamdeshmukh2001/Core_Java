6203ee0900484e6338af729fd32e71885054b1eb
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-05-05 23:56:08 +0530

    pattern

---
+++ Patterns/No_Pattern1.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class No_Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row :");
+        int r = sc.nextInt();
+        System.out.print("Enter column :");
+        int c = sc.nextInt();
+
+
+        for (int i = 1; i <= r ; i++) {
+            for (int j = 1; j <= c; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+            
+        }
+        sc.close();
+    }
+}
