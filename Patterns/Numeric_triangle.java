120b4af53ac99908a4ccea3c5de60d773eb8f4ad
ShivamDeshmukh <sadeshmukh2001@gmail.com>
2024-05-06 00:18:32 +0530

    pattern

---
+++ Patterns/Numeric_triangle.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+
+public class Numeric_triangle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row :");
+        int r = sc.nextInt();
+        
+        
+        
+        for(int i=1; i<=r; i++){
+            for(int j=1; j <= i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+            }
+            
+        }
+}
+
